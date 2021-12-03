b26b52df656b523e27b3120e8e023ab8e37b7da8
Renaud77 <78032548+Renaud77@users.noreply.github.com>
2021-12-03 17:47:30 +0000

    mise en place du projet

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const userRoutes = require("./routes/user");
+const sauceRoutes = require("./routes/sauce");
+
+// connecting to the data base
+mongoose
+  .connect(
+    "mongodb+srv://renaud_77:Monsters77@cluster0.1gtrq.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+const app = express();
+
+// CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/sauces", sauceRoutes);
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
